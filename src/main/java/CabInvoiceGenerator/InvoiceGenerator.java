efcb6f45866e19d77475cd6539f4701de303056e
jangle <shubhamjangale0000@gmail.com>
2020-10-26 07:50:16 +0530

    Added : Welcome to Invoice Generator Service

---
+++ src/main/java/CabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,9 @@
+package CabInvoiceGenerator;
+
+public class InvoiceGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Invoice Generator Service");
+	}
+
+}
